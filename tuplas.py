9630e9d796b6824bffb57977917b3774f35b3dae
Maria Jose <lopezalvaradomariajosee6.5@gmail.com>
2025-05-24 17:37:12 -0500

    Nuevo documento

---
+++ tuplas.py
@@ -0,0 +1,120 @@
+print("Indicaciones:")
+print("Ejercicio 1")
+tupla1 = (1,2,3,4,5)
+print("Ejercicio 2")
+print(tupla1[1])
+print("Ejercicio 3")
+print("longitud" ,len(tupla1))
+print("Ejercicio ")
+print("posicion" ,tupla1.index(3))
+print("Ejercicio 5")
+print("Veces" ,tupla1.count(2))
+print("Ejercicio 6")
+tup=("ministerio" ,2,1.70)
+print(tup)
+print("Ejercicio 7")
+tupla2=(tupla1)
+print("valor interno" ,tupla1[0:2])
+
+print("**************************************************************************")
+print("ACTIVIDAD")
+print("Ejercicio 1")
+tupla=(1,2,3,4,5)
+print("Primer elemento: " ,tupla[0] ,"Ultimo elemento: " ,tupla[-1])
+
+print("Ejercicio 2")
+lista=[1.34,1.55,0.35,10.65]
+print("Segundo elemento: " ,lista[1] ,"Cuarto elemento: " ,lista[-2])
+
+print("Ejercicio 3")
+tupla2=("Ministerio","de","trabajo")
+
+print("Ejercicio 4")
+lista2=[6,7,8,9,3]
+suma1= lista2[0]+lista2[1]+lista2[2]+lista2[3]+lista2[4]
+print("La suma es: " ,suma1)
+
+print("Ejercicio 5")
+tupla3=(1.23,75.09,3.14)
+suma3=tupla3[0]+tupla3[1]+tupla3[2]
+prome=suma3//3
+print("El promedio es: " ,prome)
+
+print("Ejercicio 6")
+
+print("Ejercicio 7")
+tup4=(4,9)
+m=tup4[0]*tup4[1]
+print("Multiplicacion: " ,m)
+
+print("Ejercicio 8")
+list=[3,2,1]
+list.append(int(input("Ingresa el numero: ")))
+print(list)
+print("Primer elemento: " ,list[0] ,"Ultimo elemento" ,list[-1])
+
+print("Ejercicio 9")
+t5=(4,5,6,7)
+oper=t5[0]*t5[1]
+print("La multiplicacion de los primeros elementos es: " ,oper)
+
+print("Ejercicio 10")
+li5=[10,20,30,12,85]
+op=li5[1]-li5[3]
+print("La diferencia de los primeros elementos es: " ,op)
+
+print("Ejercicio 11")
+tup=(6,15,28,49,25)
+mul=tup[0]*tup[-1]
+print("El resultado de la multiplicacion del primer y segundo numero es: " ,mul)
+
+print("Ejercicio 12")
+lista5=[40,20,30,12,85]
+opera=lista5[0]-lista5[2]
+print("La División de el primer y tercer numero es: " ,opera)
+
+print("Ejercicio 13")
+tu7 = (10, 20, 30, 40)
+el3 =tu7[2]
+print("El tercer elemento es: " ,el3)
+
+print("Ejercico 14")
+nflot = [1.2, 3.4, 5.6, 7.8, 9.0]
+su = sum(nflot)
+print("La suma de los números flotantes es: " ,su)
+
+print("Ejercicio 15")
+liste= [11, 22, 33, 44]
+tupe = tuple(liste)
+print("La tupla resultante es: " ,tupe)
+
+print("Ejercicio 16")
+
+t = (21, 32, 43)
+li= len(t)
+li.append(4)
+print("Lista después de agregar un nuevo número: " ,li)
+
+print("Ejercicio 17")
+l = [80,58,16,40,317]
+tp = tuple(l)
+print("El tercer elemento de la tupla es: " ,tp[2])
+
+print("Ejercicio 18")
+tup = (100,50,25)
+list = list(tup)
+on = int(input("Introduce otro numero para reemplazar:"))
+list[1] = on
+print("La lista despues de ser reemplazada" ,list)
+
+print("Ejercicio 19")
+lit = [5, 10, 15, 20]
+t = tuple(lit)
+print("La tupla tiene:",lit(t),"elementos")
+
+print("Ejercio 20")
+tpl = (101, 200, 300, 400, 500)
+lis = list(tpl)
+list.pop()
+print("Lista después de eliminar el último elemento: " ,list)
+

e19e2affd1146faa6f204b815424fab5f856665f
Maria Jose <lopezalvaradomariajosee6.5@gmail.com>
2025-06-05 15:49:28 -0500

    n

---
+++ diagrama_de_flujo1.py
@@ -0,0 +1,9 @@
+print("Credito bancario")
+
+nom=input("Ingrese su nombre")
+edad = int(input("Ingrese su edad:"))
+
+if edad>=18:
+    print("Puede acceder a un credito")
+else:
+    print("No puede acceder a un credito")
